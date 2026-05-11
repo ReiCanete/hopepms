7597c83cb64574a846e4377c5d782567a60511d2
Rei Cañete <reiangelo.canete@neu.edu.ph>
2026-05-12 02:19:46 +0800

    fix: tailwind config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
-
